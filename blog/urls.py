c0f585a26557efee3fa330f3ef8edb7f418471c1
sambergeron <sbergeron101@gmail.com>
2014-09-09 23:51:01 +0000

    serve static content from url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from .views import blogPageView, postDetailView
+from . import settings
 
 # for blog views
 urlpatterns = patterns('blog.views',
@@ -8,4 +9,10 @@ urlpatterns = patterns('blog.views',
     #for the blog view
     url(r'^/$', blogPageView.as_view(), name='main'),
     url(r'^/(?P<slug>[\w-]+)/$', postDetailView.as_view(), name='post_detail'),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
